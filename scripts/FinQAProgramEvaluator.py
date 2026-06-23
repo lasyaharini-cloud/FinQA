443b7634e8a5395e5e51fb4c9ba5f018897a297f
Joseph Pottern <josephpottern@gmail.com>
2026-06-23 10:48:21 -0400

    Update for repeated use (function)

--- scripts/FinQAProgramEvaluator.py
+++ scripts/FinQAProgramEvaluator.py
@@ -6,63 +6,73 @@
 #Example: msg = divide(3,10), multiply(#0,CONST_100)
 #This should return 30. Space after comma is optional.
 
-#Operations list
-ops = ["add","subtract","multiply","divide"] 
-trueops = ["+","-","*","/"]
-
-#Input message
-message = "subtract(219,175), subtract(175,#0), divide(#1,175)"
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-#Get rid of last ")"
-msg = message[0:len(message)-1] 
-
-#Remove spaces
-msg = msg.replace(" ","")
-
-#Split between operations (steps)
-steps = msg.split("),")
-
-#Initialize memory to list of "NA"
-memory = ["NA"]*len(steps)
-
-#Function to evaluate operation (step)
-def stepeval(step:str):
+def EvalProgram(message):    
+    #Operations list
+    ops = ["add","subtract","multiply","divide","exp", "greater"] 
+    trueops = ["+","-","*","/","**",">"]
     
-    #Split between op and args
-    argop = step.split("(")
+    #Input message
+    #message = "divide(16,2), greater(9,#0)"
     
-    #Extract operation symbol (in python)
-    opstr = argop[0]
-    opI = ops.index(opstr)
-    trueop = trueops[opI]
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
     
-    #Extract each arg
-    argsstr = argop[1]
-    [arg1str, arg2str] = argsstr.split(",")
+    #Get rid of last ")"
+    msg = message[0:len(message)-1] 
     
-    #Turn memory (#) or constants (CONST_) into the correct number
-    if "#" in arg1str:
-        I1 = eval(arg1str[1:len(arg1str)])
-        arg1str = str(memory[I1])
-    elif "CONST_" in arg1str:
-        conspl1 = arg1str.split("_")
-        arg1str = conspl1[1]
+    #Remove spaces
+    msg = msg.replace(" ","")
+    
+    #Place minus sign for potential M1 (minus one)
+    msg = msg.replace("M","-")
+    
+    #Split between operations (steps)
+    steps = msg.split("),")
+    
+    #Initialize memory to list of "NA"
+    memory = ["NA"]*len(steps)
+    
+    ##############
+    #Function to evaluate operation (step)
+    def stepeval(step:str):
         
-    if "#" in arg2str:
-        I2 = eval(arg2str[1:len(arg2str)])
-        arg2str = str(memory[I2])
-    elif "CONST_" in arg2str:
-        conspl2 = arg2str.split("_")
-        arg2str = conspl2[1]
+        #Split between op and args
+        argop = step.split("(")
+        
+        #Extract operation symbol (in python)
+        opstr = argop[0]
+        opI = ops.index(opstr)
+        trueop = trueops[opI]
+        
+        #Extract each arg
+        argsstr = argop[1]
+        [arg1str, arg2str] = argsstr.split(",")
+        
+        #Turn memory (#) or constants (CONST_) into the correct number
+        if "#" in arg1str:
+            I1 = eval(arg1str[1:len(arg1str)])
+            arg1str = str(memory[I1])
+        elif "CONST_" in arg1str:
+            conspl1 = arg1str.split("_")
+            arg1str = conspl1[1]
+            
+        if "#" in arg2str:
+            I2 = eval(arg2str[1:len(arg2str)])
+            arg2str = str(memory[I2])
+        elif "CONST_" in arg2str:
+            conspl2 = arg2str.split("_")
+            arg2str = conspl2[1]
+        
+        #Return arg1 (operation) arg2 as the answer
+        ans = eval(arg1str + trueop + arg2str)
+        return ans
+    ##################
     
-    #Return arg1 (operation) arg2 as the answer
-    ans = eval(arg1str + trueop + arg2str)
-    return ans
+    #Call iteratively to fill up the memory
+    for i in range(0,len(steps)):
+        memory[i] = stepeval(steps[i])
+    
+    return memory[-1]
 
-#"Call main"
-for i in range(0,len(steps)):
-    memory[i] = stepeval(steps[i])
-
-print(memory[-1])
+if __name__ == "__main__":
+    import sys
+    EvalProgram(sys.argv)
